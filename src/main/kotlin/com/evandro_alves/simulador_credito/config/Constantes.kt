f26123369dcad959aec6580e3cf6ff1e1a17984d
Evandro Alves <evandroacer3@gmail.com>
2024-12-20 16:38:09 -0300

    refactor: criado pacote de constantes da aplicação

---
+++ src/main/kotlin/com/evandro_alves/simulador_credito/config/Constantes.kt
@@ -0,0 +1,13 @@
+package com.evandro_alves.simulador_credito.config
+
+import java.math.BigDecimal
+
+object Constantes {
+
+    val TAXAJUROSATE25: BigDecimal = BigDecimal(5)
+    val TAXAJUROSATE40: BigDecimal = BigDecimal(3)
+    val TAXAJUROSATE60: BigDecimal = BigDecimal(2)
+    val TAXAJUROSACIMA60: BigDecimal = BigDecimal(4)
+    val ANO: BigDecimal = BigDecimal(12)
+
+}
