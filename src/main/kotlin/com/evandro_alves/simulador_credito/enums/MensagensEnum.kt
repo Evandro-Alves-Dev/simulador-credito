2aae96c2f6fc23c6b05c988042c09d183713e606
Evandro Alves <evandroacer3@gmail.com>
2024-12-20 16:35:46 -0300

    refactor: criado pacote de enum da aplicação

---
+++ src/main/kotlin/com/evandro_alves/simulador_credito/enums/MensagensEnum.kt
@@ -0,0 +1,8 @@
+package com.evandro_alves.simulador_credito.enums
+
+enum class MensagensEnum(val descricao:String) {
+
+    INI_PRO("Iniciado processamento"),
+    FIM_PRO("Finalizado processamento"),
+    VLR_EMP("Valor empréstimo não deve ser informado")
+}
