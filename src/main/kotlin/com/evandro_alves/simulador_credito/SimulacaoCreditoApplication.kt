1c9163d7564f1d4f996a2cbdb8dcddc3722b635d
Evandro Alves <evandroacer3@gmail.com>
2024-12-20 16:32:50 -0300

    refactor: criado configuração base do projeto

---
+++ src/main/kotlin/com/evandro_alves/simulador_credito/SimulacaoCreditoApplication.kt
@@ -0,0 +1,11 @@
+package com.evandro_alves.simulador_credito
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SimulacaoCreditoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SimulacaoCreditoApplication>(*args)
+}
---
+++ src/test/kotlin/com/evandro_alves/simulador_credito/SimuladorDeCreditoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.evandro_alves.simulador_credito
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SimuladorDeCreditoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
