e6f6bd01940923fa08829e163b359ff1cf1a5e01
Evandro Alves <evandroacer3@gmail.com>
2024-12-27 12:30:44 -0300

    chore: ajuste enum

--- src/main/kotlin/com/evandro_alves/simulador_credito/enums/TipoSimulacaoEnum.kt
+++ src/main/kotlin/com/evandro_alves/simulador_credito/enums/TipoSimulacaoEnum.kt
@@ -1,6 +1,6 @@
 package com.evandro_alves.simulador_credito.enums
 
-enum class TipoSimulacaoEnum(codigo: String, descricao: String) {
+enum class TipoSimulacaoEnum(private val codigo: String, private val descricao: String) {
     SVP("SVP","Simulação por valor parcela"),
     SDN("SDN","Simulação por data de nascimento"),
     SQP("SQP", "Simulação por quantidade de parcelas");
